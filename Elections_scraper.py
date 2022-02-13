bbdeb60ee6483c7ed5201194b8d28af4dc923878
Roman Németh <romannemeth1@gmail.com>
2022-02-14 00:54:46 +0100

    Current status of the project

--- Elections_scraper.py
+++ Elections_scraper.py
@@ -1,5 +1,6 @@
+import csv
 import sys
-from bs4 import BeautifulSoup as BeSo
+import bs4
 import requests
 
 
@@ -13,27 +14,98 @@ def arg_check() -> tuple:
         csv_n = sys.argv[2]
         response = requests.get(url)
         if csv_n[-4:] != ".csv":
-            print(f"Skontroluj správnosť názvu súboru. Názov sa musí končiť výrazov \".csv\"")
+            print(f"Skontroluj správnosť názvu súboru. Názov sa musí končiť výrazom \".csv\"")
         if not response.ok:
             print(f"Skontroluj správnosť linku. Odpoveď serveru: \"{response.status_code}: {response.reason}\"")
             sys.exit()
 
-    return csv_n, response
+    return csv_n, response.text
 
 
-# making BeautifulSoup
-def make_soup(response) -> BeSo.BeautifulSoup:
-    soup_result = BeSo(response.text, "html.parser")
-    return soup_result
+# creating clean csv file with header
+def csv_creation(filename) -> None:
+    header = (
+        "Kód obce", "Název obce", "Voliči v seznamu", "Vydané obálky", "Platné hlasy", "Kandidující strany",
+        "Občanská demokratická strana", "Řád národa - Vlastenecká unie", "CESTA ODPOVĚDNÉ SPOLEČNOSTI",
+        "Česká str.sociálně demokrat.", "Radostné Česko", "STAROSTOVÉ A NEZÁVISLÍ",
+        "Komunistická str.Čech a Moravy", "Strana zelených", "ROZUMNÍ-stop migraci,diktát.EU",
+        "Strana svobodných občanů", "Blok proti islam.-Obran.domova", "Občanská demokratická aliance",
+        "Česká pirátská strana", "OBČANÉ 2011-SPRAVEDL. PRO LIDI", "Referendum o Evropské unii", "TOP 09",
+        "ANO 2011", "SPR-Republ.str.Čsl. M.Sládka", "Křesť.demokr.unie-Čs.str.lid.",
+        "Česká strana národně sociální", "REALISTÉ", "SPORTOVCI", "Dělnic.str.sociální spravedl.",
+        "Svob.a př.dem.-T.Okamura (SPD)", "Strana Práv Občanů"
+    )
+    with open(filename, "w", newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(header)
+    return None
 
 
-# creating csv file
-def make_csv():
+# Scraping of commune links list
+def links_to_scrape(beso) -> list:
+    links = []
+    tables_to_scrape = beso.find_all("table")
+    for table in tables_to_scrape:
+        rows = table.children
+        for row in filter(lambda x: x != '\n', rows):
+            for cell in row.find_all("td"):
+                try:
+                    class_in = cell.attrs.get("class")[-1]
+                except TypeError:
+                    class_in = [cell.attrs.get("class")]
+                if cell.attrs.get("headers") is not None:
+                    if class_in == "cislo" and cell.attrs.get("headers")[-1][-3:] == "sb1":
+                        links.append([])
+                        links[-1].append(cell.a.string)
+                        links[-1].append("https://volby.cz/pls/ps2017nss/" + cell.a.attrs.get("href"))
+                    elif cell.attrs.get("headers")[-1][-3:] == "sb2" and (cell.a is not None):
+                        links[-1].append(cell.a.string)
+                    elif cell.attrs.get("headers")[-1][-3:] == "sb2" and class_in == "overflow_name":
+                        links[-1].append(cell.string)
+    return links
+
+
+# Commune data scraping
+def obec_scrape(link_list) -> list:
+    link = link_list[1]
+    row_to_add = [link_list[0], link_list[2]]
+    response = requests.get(link)
+    if not response.ok:
+        raise requests.HTTPError(f"""Jedna z URLs na \"volby.cz\" nefunguje: \"{response.status_code}: "
+                                 {response.reason}\"""")
+
+    soup_com = bs4.BeautifulSoup(response.text, "html.parser")
+    tables_to_scrape = soup_com.find_all("table")
+    for table in tables_to_scrape:
+        rows = table.children
+        for row in filter(lambda x: x != '\n', rows):
+            for cell in row.find_all("td"):
+                if (cell.attrs.get("class") is not None) or (cell.attrs.get("headers") is not None):
+                    if cell.attrs.get("class")[0] == "cislo" and cell.attrs.get("headers")[0] == "sa2":
+                        row_to_add.append(cell.string)
+                    elif cell.attrs.get("class")[0] == "cislo" and cell.attrs.get("headers")[0] == "sa3":
+                        row_to_add.append(cell.string)
+                    elif cell.attrs.get("class")[0] == "cislo" and cell.attrs.get("headers")[0] == "t1sa2" \
+                            and cell.attrs.get("headers")[1] == "t1sb3":
+                        row_to_add.append(cell.string)
+    print(row_to_add)
+    return row_to_add
+
+
+# adding scraped communes
+def row_adding(filename, row) -> None:
+    with open(filename, "a", newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(row)
     return None
 
 
 if __name__ == "__main__":
-    csv_name = arg_check()[0]
-    resp = arg_check()[1]
-    print(resp)
-    soup = make_soup(resp)
+    csv_name, resp = arg_check()
+    soup = bs4.BeautifulSoup(resp, "html.parser")
+    csv_creation(csv_name)
+    links_list = links_to_scrape(soup)
+    print(links_list)
+    for r in range(0, len(links_list)):
+        row_t_add = obec_scrape(links_list[r])
+        row_adding(csv_name, row_t_add)
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# engeto-projekt-3
+Výsledky hlasovani pre okres Plzeň-město:
+   
+1. argument: https://volby.cz/pls/ps2017nss/ps32?xjazyk=CZ&xkraj=4&xnumnuts=3203
+2. argument: vysledky_Plzen.csv
+
+Spustenie programu:
+
+python Elections_scraper.py "https://volby.cz/pls/ps2017nss/ps32?xjazyk=CZ&xkraj=4&xnumnuts=3203" "vysledky_plzen.csv"
+
+Priebeh sťahovani:
+
+
+
+Čiastočný výstup:
