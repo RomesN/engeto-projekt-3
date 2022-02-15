072e0613eb771dff94e73e81e33ae9731253f20f
Roman Németh <romannemeth1@gmail.com>
2022-02-15 21:36:03 +0100

    Final version

--- Elections_scraper.py
+++ Elections_scraper.py
@@ -6,6 +6,7 @@ import requests
 
 # aguments check
 def arg_check() -> tuple:
+    # number of arguments
     if len(sys.argv) != 3:
         sys.exit("""Nesprávny počet argumentov! Správne použitie: python Elections_scraper.py
                   \"LINK\" \"JMÉNO_SOUBORU.csv\"""")
@@ -13,8 +14,11 @@ def arg_check() -> tuple:
         url = sys.argv[1]
         csv_n = sys.argv[2]
         response = requests.get(url)
+        # csv name on the right place
         if csv_n[-4:] != ".csv":
             print(f"Skontroluj správnosť názvu súboru. Názov sa musí končiť výrazom \".csv\"")
+            sys.exit()
+        # url validity
         if not response.ok:
             print(f"Skontroluj správnosť linku. Odpoveď serveru: \"{response.status_code}: {response.reason}\"")
             sys.exit()
@@ -51,16 +55,21 @@ def links_to_scrape(beso) -> list:
             for cell in row.find_all("td"):
                 try:
                     class_in = cell.attrs.get("class")[-1]
+                    header_in = cell.attrs.get("headers")[-1][-3:]
                 except TypeError:
                     class_in = [cell.attrs.get("class")]
+                    header_in = cell.attrs.get("headers")[-1][-3:]
                 if cell.attrs.get("headers") is not None:
-                    if class_in == "cislo" and cell.attrs.get("headers")[-1][-3:] == "sb1":
+                    # Scraping commune number
+                    if class_in == "cislo" and header_in == "sb1":
                         links.append([])
                         links[-1].append(cell.a.string)
                         links[-1].append("https://volby.cz/pls/ps2017nss/" + cell.a.attrs.get("href"))
-                    elif cell.attrs.get("headers")[-1][-3:] == "sb2" and (cell.a is not None):
+                    # Scraping commune name if it is link
+                    elif header_in == "sb2" and (cell.a is not None):
                         links[-1].append(cell.a.string)
-                    elif cell.attrs.get("headers")[-1][-3:] == "sb2" and class_in == "overflow_name":
+                    # Scraping commune name
+                    elif header_in == "sb2" and class_in == "overflow_name":
                         links[-1].append(cell.string)
     return links
 
@@ -73,27 +82,31 @@ def obec_scrape(link_list) -> list:
     if not response.ok:
         raise requests.HTTPError(f"""Jedna z URLs na \"volby.cz\" nefunguje: \"{response.status_code}: "
                                  {response.reason}\"""")
-
     soup_com = bs4.BeautifulSoup(response.text, "html.parser")
     tables_to_scrape = soup_com.find_all("table")
     for table in tables_to_scrape:
         rows = table.children
         for row in filter(lambda x: x != '\n', rows):
             for cell in row.find_all("td"):
+                cell_cl = cell.attrs.get("class")[0]
+                cell_he = cell.attrs.get("headers")[-1]
                 if (cell.attrs.get("class") is not None) or (cell.attrs.get("headers") is not None):
-                    if cell.attrs.get("class")[0] == "cislo" and cell.attrs.get("headers")[-1] == "sa2":
-                        row_to_add.append(cell.string)
-                    elif cell.attrs.get("class")[0] == "cislo" and cell.attrs.get("headers")[-1] == "sa3":
-                        row_to_add.append(cell.string)
-                    elif cell.attrs.get("class")[0] == "cislo" and cell.attrs.get("headers")[-1] == "sa6":
-                        row_to_add.append(cell.string)
-                    elif cell.attrs.get("class")[0] == "cislo" and ((cell.attrs.get("headers")[-1] == "t2sb3") or
-                                                                    (cell.attrs.get("headers")[-1] == "t1sb3")):
-                        row_to_add.append(cell.string)
+                    # Scraping "Voliči v seznamu"
+                    if cell_cl == "cislo" and cell_he == "sa2":
+                        row_to_add.append(cell.string.replace("\xa0", ""))
+                    # Scraping "Vydané obálky"
+                    elif cell_cl == "cislo" and cell_he == "sa3":
+                        row_to_add.append(cell.string.replace("\xa0", ""))
+                    # Scraping "Platné hlasy"
+                    elif cell_cl == "cislo" and cell_he == "sa6":
+                        row_to_add.append(cell.string.replace("\xa0", ""))
+                    # Scraping "Hlasy strany celkem"
+                    elif cell_cl == "cislo" and ((cell_he == "t2sb3") or (cell_he == "t1sb3")):
+                        row_to_add.append(cell.string.replace("\xa0", ""))
     return row_to_add
 
 
-# adding scraped communes
+# adding data from scraped communes into the file
 def row_adding(filename, row) -> None:
     with open(filename, "a", newline='') as f:
         writer = csv.writer(f)
--- README.md
+++ README.md
@@ -5,10 +5,16 @@ Výsledky hlasovani pre okres Plzeň-město:
 
 Spustenie programu:
 
-python Elections_scraper.py "https://volby.cz/pls/ps2017nss/ps32?xjazyk=CZ&xkraj=4&xnumnuts=3203" "vysledky_plzen.csv"
+> python Elections_scraper.py "https://volby.cz/pls/ps2017nss/ps32?xjazyk=CZ&xkraj=4&xnumnuts=3203" "vysledky_plzen.csv"
 
-Priebeh sťahovani:
+Priebeh sťahovania:
 
-
+> SŤAHUJEM Z VYBRANÉHO URL: https://volby.cz/pls/ps2017nss/ps32?xjazyk=CZ&xkraj=4&xnumnuts=3203
+> UKLADÁM DO SÚBORU: vysledky_Plzen.csv
+> KONČÍM Election-scraper.
 
 Čiastočný výstup:
+
+> Kód obce,Název obce,Voliči v seznamu,Vydané obálky,Platné hlasy,Občanská demokratická strana,Řád národa - Vlastenecká unie,CESTA ODPOVĚDNÉ SPOLEČNOSTI,Česká str.sociálně demokrat.,Radostné Česko,STAROSTOVÉ A NEZÁVISLÍ,Komunistická str.Čech a Moravy,Strana zelených,"ROZUMNÍ-stop migraci,diktát.EU",Strana svobodných občanů,Blok proti islam.-Obran.domova,Občanská demokratická aliance,Česká pirátská strana,OBČANÉ 2011-SPRAVEDL. PRO LIDI,Referendum o Evropské unii,TOP 09,ANO 2011,SPR-Republ.str.Čsl. M.Sládka,Křesť.demokr.unie-Čs.str.lid.,Česká strana národně sociální,REALISTÉ,SPORTOVCI,Dělnic.str.sociální spravedl.,Svob.a př.dem.-T.Okamura (SPD),Strana Práv Občanů
+> 558851,Dýšina,1349,860,853,114,0,0,48,0,52,41,10,5,16,1,2,119,0,3,45,269,5,34,0,3,2,1,80,3
+> 558966,Chrást,1429,1002,999,151,1,1,51,1,31,63,8,4,15,1,2,111,1,1,45,354,1,24,0,11,5,2,114,1 
